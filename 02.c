e16e634e239e50005f22d7bec0d9a5db9591cfbc
Dev Jadiya <devjadiya15@gmail.com>
2023-03-01 21:33:13 +0530

    DAY-1

---
+++ 02.c
@@ -0,0 +1,27 @@
+// Escape sequences \t
+
+#include<stdio.h>
+int main()
+{
+printf("Hello git!\t");
+printf("I am writing c codes");
+return 0;
+}
+
+// Backslash
+#include<stdio.h>
+int main()
+{
+    printf("Hello gits\\");
+    printf("I am writing c codes");
+return 0;
+}
+
+// Inserting a double quote character 
+#include<stdio.h>
+int main()
+{
+    printf("They call me \"dev\"");
+    return 0;
+    
+}
---
+++ 04.c
@@ -0,0 +1,16 @@
+//For comments double forward slashes and slashes with asterisk /* */ are used
+// Creating variables
+#include<stdio.h>
+int main()
+{
+int num = 10;
+float floatnum = 9.81;
+char letter = 'S';
+
+printf("%d\n", num);
+printf("%f\n", floatnum);
+printf("%c\n",letter);
+
+return 0;
+}
+
