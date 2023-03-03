920470b1a56855414699696c8777c34c7185cc47
devjadiya <121892398+devjadiya@users.noreply.github.com>
2023-03-03 23:01:24 +0530

    DAY-3

---
+++ 06.c
@@ -0,0 +1,14 @@
+// Boolean Variable
+#include <stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool iscodingfun = true;
+    bool islifebad = false;
+    // created boolean function
+
+    // returning boolean values
+    printf("%d\n", iscodingfun);
+    printf("%d\n", islifebad);
+    return 0;
+}
---
+++ 07.c
@@ -0,0 +1,17 @@
+// If else
+#include<stdio.h>
+int main()
+{
+    int time = 40;
+    if (time < 28)
+    {
+        printf("Good day.");
+    }
+        else
+        {
+            printf("Good evening.");
+        }
+        return 0;
+    }
+
+
