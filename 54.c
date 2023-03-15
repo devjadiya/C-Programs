7ad3f8f2bc5ed7839b65cc284c31f4c04416389b
devjadiya <121892398+devjadiya@users.noreply.github.com>
2023-03-15 23:06:34 +0530

    DAY-15

---
+++ 54.c
@@ -0,0 +1,22 @@
+// C Structures
+// We can use the strcpy() function and assign the value to s1.myString
+#include <stdio.h>
+#include <string.h>
+
+struct myStructure {
+  int myNum;
+  char myLetter;
+  char myString[30];  // String
+};
+
+int main() {
+  struct myStructure s1;
+
+  // Assign a value to the string using the strcpy function
+  strcpy(s1.myString, "Some text");
+
+  // Print the value
+  printf("My string: %s", s1.myString);
+
+  return 0;
+}
